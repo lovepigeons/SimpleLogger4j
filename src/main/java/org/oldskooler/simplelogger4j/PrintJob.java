415cefa9cd76e8d73433f8b4878eb231ecc01f04
Alexandra Miller-Blake <alex.millerblake@outlook.com>
2025-09-26 09:36:05 +1000

    Update PrintJob.java

--- src/main/java/org/oldskooler/simplelogger4j/PrintJob.java
+++ src/main/java/org/oldskooler/simplelogger4j/PrintJob.java
@@ -10,13 +10,11 @@ public class PrintJob {
     final LocalDateTime timestamp;
     final Throwable throwable;
     final boolean isFlush;
-    final String className;
-    final String packageName;
+    final String name;
 
-    public PrintJob(String className, String packageName, long seq, LogLevel lvl, String msg,
+    public PrintJob(String name, long seq, LogLevel lvl, String msg,
                     String thread, LocalDateTime ts, Throwable th, boolean flush) {
-        this.className = className;
-        this.packageName = packageName;
+        this.name = name;
         this.sequence = seq;
         this.level = lvl;
         this.message = msg;
@@ -26,13 +24,13 @@ public class PrintJob {
         this.isFlush = flush;
     }
 
-    static PrintJob of(String className, String packageName, LogLevel lvl, String msg, Throwable th) {
+    static PrintJob of(String name, LogLevel lvl, String msg, Throwable th) {
         long s = LoggerBus.nextSeq();
-        return new PrintJob(className, packageName, s, lvl, msg, Thread.currentThread().getName(),
+        return new PrintJob(name, s, lvl, msg, Thread.currentThread().getName(),
                 LocalDateTime.now(), th, false);
     }
 
     static PrintJob flush() {
-        return new PrintJob(null, null, -1, LogLevel.INFO, null, "flush", LocalDateTime.now(), null, true);
+        return new PrintJob(null, -1, LogLevel.INFO, null, "flush", LocalDateTime.now(), null, true);
     }
 }
